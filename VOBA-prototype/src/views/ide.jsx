87cc4ff72093039c5caa65cb0e1b20963a522cbc
TJ Koonce <tj7@vt.edu>
2023-04-22 15:35:35 -0400

    ide page update, missing header and pop-up for editing tests

--- VOBA-prototype/src/views/ide.jsx
+++ VOBA-prototype/src/views/ide.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import IDETest from "../components/IDETest";
 
 function IDE() {
+    var sample_tests = [
+        {
+            title: 'Audio Enhancement'
+        },
+        {
+            title: 'Audio Masking'
+        },
+        {
+            title: 'Blockchain Encryption'
+        },
+        {
+            title: 'User Authentication'
+        }
+    ]
   return (
     <div class="page">
         <div class="horizontal-line vertical-line tests-ide" style={{height: "100vh"}}>
@@ -14,8 +29,13 @@ function IDE() {
                 <FontAwesomeIcon icon={faPenToSquare} />
                 <span>Edit Tests</span>
             </button>
-            <div class="flex justify-center items-center">
-                Active Tests
+            <div class="test-list">
+                <div class="test">
+                    <div className="text-xl">Current Tests</div>
+                    {sample_tests.map((sample, index) => {
+                        return <IDETest key={index} title={sample.title}/>
+                    })}
+                </div>
             </div>
         </div>
     </div>

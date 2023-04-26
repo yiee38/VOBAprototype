ce38a35cec3a65924cf27adfa5e31b1dcc7dc5ba
mchenyu <mchenyu@vt.edu>
2023-04-25 21:10:11 -0400

    Filter implementation

--- VOBA-prototype/src/views/database.jsx
+++ VOBA-prototype/src/views/database.jsx
@@ -12,7 +12,46 @@ import SearchListItem from '../components/SearchListItem';
 function Database () {
   const [newTestbed, setNewTestbed] = useState(false)
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sample, setSample] = useState([
+    {title: 'Report 1', date: '4/12/23', tests:'Audio enhancement, Text2Voice'},
+    {title: 'Report 2', date: '4/12/23', tests:'User Authentication, Firewall'},
+    {title: 'Report 3', date: '4/12/23', tests:'Blockchain Encryption, Text2Voice'},
+    {title: 'Report 4', date: '4/12/23', tests:'Voice2Voice'},
+    {title: 'Report 5', date: '4/12/23', tests:'Audio File Clearance'},
+    {title: 'Report 6', date: '4/12/23', tests:'Voice2Text, Audio Masking'},
+  ]);
+
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+    // checkbox
+    const [checkedItems, setCheckedItems] = useState([]);
+
+    const handleCheckboxChange = (event) => {
+      const value = event.target.value;
+      if (event.target.checked) {
+        setCheckedItems([...checkedItems, value]);
+        console.log(checkedItems)
+      } else {
+        setCheckedItems(checkedItems.filter((item) => item !== value));
+        console.log(checkedItems)
+      }
+    };
+
+  
+    const filteredList = checkedItems.length === 0
+    ? sample // if checkedItems is empty, return the original list
+    : sample.filter((item) =>
+      checkedItems.some(word =>
+        item.tests.toLowerCase().includes(word.toLowerCase())
+    ));
+  
+
   const navigate = useNavigate()
+
 
   var sample_test = [
     {
@@ -44,16 +83,6 @@ function Database () {
     },
   ]
 
-  var sample_results = [
-    {title: 'Report 1', date: '4/12/23', tests:'Audio enhancement, Text2Voice'},
-    {title: 'Report 2', date: '4/12/23', tests:'User Authentication, Firewall'},
-    {title: 'Report 3', date: '4/12/23', tests:'Blockchain Encryption, Text2Voice'},
-    {title: 'Report 4', date: '4/12/23', tests:'Voice2Voice'},
-    {title: 'Report 5', date: '4/12/23', tests:'Audio File Clearance'},
-    {title: 'Report 6', date: '4/12/23', tests:'Voice2Text, Audio Masking'},
-
-  ]
-
   return (
     <div className="flex flex-1 h-full w-full flex-col pt-10 px-10"> 
       <div className="flex flex-col w-full gap-8">
@@ -61,18 +90,21 @@ function Database () {
           <div className='text-lg'>Filters</div>
           <div className='flex flex-col gap-4 w-full'>
             {sample_test.map((test, index) => {
-              return <FilterBox key={index} category={test.title} tests={test.task_list}/>
+              return <FilterBox key={index} category={test.title} tests={test.task_list} handleFun = {handleCheckboxChange}/>
             })}
           </div>
         </div>
+
         <div>
           <div className='text-lg'>Results</div>
           <div className='flex flex-col gap-4 w-full'>
-            {sample_results.map((result, index) => {
+            {filteredList.map((result, index) => {
               return <SearchListItem title={result.title} date={result.date} tests={result.tests} />
             })}
           </div>
         </div>
+        <p>Checked items: {checkedItems.join(', ')}</p>
+
       </div>
     </div>
   )
